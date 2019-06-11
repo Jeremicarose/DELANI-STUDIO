138930cf015ab5b3d0771f3ba939c430bf3d0ddc
Jeremic <arosejeremic@gmail.com>
2019-06-11 23:49:02 +0300

    changed the toggle icon

--- js/script.js
+++ js/script.js
@@ -1,55 +1,49 @@
 $(document).ready(function () {
     $("#img1").click(function () {
         $("#par1").slideToggle();
-        $("#img1").slidetoggle("slow");
+        $("#img1").hide();
     });
 
     $("#par1").click(function () {
         $("#img1").slideToggle();
-        $("#par1").slidetoggle("slow");
+        $("#par1").hide();
     });
 
     $("#img2").click(function () {
         $("#par2").slideToggle();
-        $("#img2").slidetoggle("slow");
+        $("#img2").hide();
     });
 
     $("#par2").click(function () {
         $("#img2").slideToggle();
-        $("#par2").slidetoggle("slow");
+        $("#par2").hide();
     });
 
     $("#img3").click(function () {
         $("#par3").slideToggle();
-        $("#img3").slidetoggle("slow");
+        $("#img3").hide();
     });
 
     $("#par3").click(function () {
         $("#img3").slideToggle();
-        $("#par3").slidetoggle("slow");
+        $("#par3").hide();
     });
 
     $("#sub").click(function () {
         alert("Thanks for contacting us.We value your feedback.")
-        
+
     });
-$(".card").hover(function(){
-    $(".img-description").fadeIn(2000);
-    $(".img-description").fadeOut(2000);
 
-});
-
-$(".onTop").hover(function () {
-        // over
+    $(".onTop").hover(function () {
         $(this).css({
-            "opacity":"3",
-            "transition":"1s"
+            "opacity": "3",
+            "transition": "1s"
         })
     }, function () {
-        // out
+
         $(this).css({
-            "opacity":"0"
+            "opacity": "0"
         })
     }
-);
+    );
 });
